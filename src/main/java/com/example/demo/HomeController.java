749604a4d804b8eb7a9fe0eb804f3ead840bff5c
SoobihnKim <ksbsj92@naver.com>
2024-07-30 11:56:44 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> home() {
+        return ResponseEntity.ok()
+                .body("Server is running....");
+    }
+}
